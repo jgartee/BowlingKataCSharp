7699a9a859ad99cd03b9661143ec5c04635e39ff
jgartee <jlgartee@gmail.com>
2019-01-31 20:19:37 -0500

    Inital project setup

---
+++ BowlingKataCSharp/Game.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BowlingKataCSharp
+{
+    public class Game
+    {
+    }
+}
---
+++ BowlingKataCSharpTests/GameShould.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace BowlingKataCSharpTests
+{
+    public class GameShould
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
