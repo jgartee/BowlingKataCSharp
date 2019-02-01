9799763059c20ed14484cbebf635552e5c4bdfb0
jgartee <jlgartee@gmail.com>
2019-02-01 05:45:51 -0500

    Refactor to move score calculation to Store method instead of in Roll method.

--- BowlingKataCSharp/Game.cs
+++ BowlingKataCSharp/Game.cs
@@ -1,17 +1,21 @@
-﻿namespace BowlingKataCSharp
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace BowlingKataCSharp
 {
     public class Game
     {
+        private readonly List<int> _rolls = new List<int>();
         private int _score;
 
         public void Roll(int pins)
         {
-            _score += pins;
+            _rolls.Add(pins);
         }
 
         public int Score()
         {
-            return _score;
+            return _rolls.Sum();
         }
     }
 }
--- BowlingKataCSharpTests/GameShould.cs
+++ BowlingKataCSharpTests/GameShould.cs
@@ -18,6 +18,18 @@ namespace BowlingKataCSharpTests
                 _game.Roll(pins);
         }
 
+        [Fact(Skip = "Ignore while refactoring")]
+        public void ScoreTenPlusNextFrameFirstRollOnSpare()
+        {
+            _game.Roll(5);
+            _game.Roll(5);
+            _game.Roll(2);
+            _game.Roll(0);
+            RollMany(16, 0);
+
+            Assert.Equal(14, _game.Score());
+        }
+
         [Fact]
         public void ScoreTwentyWhenEachRollKnocksDownOnePin()
         {
